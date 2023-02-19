6fef5557f5a84b240c64dfa4e9bfcf377ed6aa6d
XanCraft21 <103157143+XanCraft21@users.noreply.github.com>
2023-02-19 13:54:48 -0500

    Add files via upload

---
+++ examples/TinyGL_Test/TinyGL_Test.ino
@@ -0,0 +1,14 @@
+#include <TinyGLDuino.h>
+
+ZBuffer *frameBuffer = ZB_open(128, 128, ZB_MODE_5R6G5B, 0);
+
+int screenWidth = 128;
+int screenHeight = 128;
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
